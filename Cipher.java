b0b055d818ee8662f5e61e27175fd6dc7c6001bb
Thomas Tran <tran.th.thomas@gmail.com>
2018-10-27 14:36:41 -0400

    Initial commit

---
+++ Cipher.java
@@ -0,0 +1,8 @@
+import algorithm.*;
+
+public class Cipher {
+  public static void main(String[] args) {
+    Caesar cipher = new Caesar();
+    cipher.test();
+  }
+}
---
+++ algorithm/Caesar.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+public class Caesar {
+  
+  public Caesar() {
+  }
+  
+  public void test() {
+    System.out.println("Hello, world!");
+  }
+}
