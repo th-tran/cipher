2aef9674bdc0d138f620b585d8b0ee8a721414e3
Thomas Tran <tran.th.thomas@gmail.com>
2018-11-03 19:47:36 -0400

    Implemented core of RSA algorithm

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import parser.*;
 import util.*;
 
 import com.beust.jcommander.JCommander;
+
 import java.util.Random;
 
 public class Main {
@@ -15,49 +16,55 @@ public class Main {
   }
     
   public void run(Settings settings) throws Exception { 
-    //System.out.println(settings.algo);
-    //System.out.println(settings.shift);
-    String msg;
+    String message;
     String launchInfo;
     switch (settings.algo) {
       case RSA:
         launchInfo = String.format("--- Starting %s ('RSA' algorithm) ---", (settings.decrypt) ? "decryption" : "encryption");
         System.out.println(launchInfo);
         RSA rsaCipher = new RSA();
-        // XXX Testing (only works with numbers right now)
-        double test = 12;
-        System.out.println(String.format("Original message: %f", test));
-        test = rsaCipher.encrypt(test, rsaCipher.e, rsaCipher.n);
-        System.out.println(String.format("Encrypted message: %f", test));
-        test = rsaCipher.decrypt(test, rsaCipher.d, rsaCipher.n);
-        System.out.println(String.format("Decrypted message: %f", test));
+
+        System.out.println("Enter the plain text:");
+        message = IOUtilities.getUserInput();
+
+        System.out.println(String.format("Encrypting string: %s", message));
+        byte[] encrypted = rsaCipher.encrypt(message.getBytes());
+        System.out.println("Encrypted message: " + encrypted.toString());
+
+        System.out.println("Decrypting string: " + encrypted.toString());
+        byte[] decrypted = rsaCipher.decrypt(encrypted);
+        System.out.println(String.format("Decrypted message: %s", new String(decrypted)));
         break;
       case SHIFT:
+        launchInfo = String.format("--- Starting %s ('Shift' algorithm, shift=%1d) ---", (settings.decrypt) ? "decryption" : "encryption", settings.shift);
+        System.out.println(launchInfo);
         if (settings.shift == 0) {
           Random random = new Random();
           int min = 1; // inclusive
           int max = 26; // exclusive
           settings.shift = random.nextInt(max - min) + min;
         }
-        launchInfo = String.format("--- Starting %s ('Shift' algorithm, shift=%1d) ---", (settings.decrypt) ? "decryption" : "encryption", settings.shift);
-        System.out.println(launchInfo);
         Shift shiftCipher = new Shift(settings.shift);
+
         if (settings.inputFile != null && !settings.inputFile.isEmpty()) {
-          msg = IOUtilities.readFromFile(settings.inputFile, true);
+          message = IOUtilities.readFromFile(settings.inputFile, true);
         } else {
           System.out.println("Enter the plain text:");
-          msg = IOUtilities.getUserInput();
+          message = IOUtilities.getUserInput();
         }
-        System.out.println("Original message: " + msg);
+
         if (settings.decrypt) {
-          msg = shiftCipher.decrypt(msg);
-          System.out.println("Decrypted message: " + msg);
+          System.out.println("Decrypting string: " + message);
+          message = shiftCipher.decrypt(message);
+          System.out.println("Decrypted message: " + message);
         } else {
-          msg = shiftCipher.encrypt(msg);
-          System.out.println("Encrypted message: " + msg);
+          System.out.println("Encrypting string: " + message);
+          message = shiftCipher.encrypt(message);
+          System.out.println("Encrypted message: " + message);
         }
+
         if (settings.outputFile != null && !settings.outputFile.isEmpty()){
-          IOUtilities.writeToFile(settings.outputFile, msg, true);
+          IOUtilities.writeToFile(settings.outputFile, message, true);
         }
         break;
       default:
--- src/cryptography/RSA.java
+++ src/cryptography/RSA.java
@@ -1,61 +1,53 @@
 package cryptography;
 
 import java.lang.Math;
+import java.math.BigInteger;
+import java.util.Random;
 
 public class RSA {
-  
-  public int d, e, n;
-  public RSA() {
-    // Two random prime numbers
-    int p = 3;
-    int q = 7;
 
-    // First part of public key
-    n = p * q;
-    // Second part of public key
-    e = 2;
-    int phi = (p - 1) * (q - 1);
-    while (e < phi) {
-      // e must be between 1 and phi (exclusive) and coprime to phi
-      if (gcd(e, phi) == 1) {
-        break;
-      } else {
-        e++;
-      }
+  public BigInteger d, e, N, p, q, phi;
+  public Random r;
+  public int bitLength;
+
+  public RSA() {
+    // Two random unique prime numbers (p and q)
+    // NOTE: If the message is larger than the modulus (N), the message recieved
+    //       after decryption will be inaccurate. Use large prime numbers to avoid this.
+    bitLength = 1024;
+    r = new Random();
+    p = BigInteger.probablePrime(bitLength, r);
+    q = BigInteger.probablePrime(bitLength, r);
+
+    // First part of public key (modulus N)
+    N = p.multiply(q);
+    // The totient (phi)
+    phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+    // Second part of public key (encryption exponent e)
+    // (1 < e < phi) AND (e is coprime to phi)
+    e = BigInteger.valueOf(2);
+    // e = BigInteger.probablePrime(bitLength / 2, r);
+    while ((e.compareTo(phi) < 0) && (e.gcd(phi).compareTo(BigInteger.ONE) > 0)) {
+        e = e.add(BigInteger.ONE);
     }
 
-    // Private key
-    // d must satisty: d * e = 1 + (k * phi)
-    int k = 2;
-    d = (1 + (k * phi)) / e;
+    // Private key (decryption exponent d)
+    // d * e = 1 + (k * phi)
+    // TODO: Try to solve for d by implementing the extended Euclid algorithm
+    d = e.modInverse(phi);
 
-    System.out.println(String.format("Public: %d , %d", n, e));
+    // TODO: Generate files to store key values instead of printing to screen
+    System.out.println(String.format("Public: %d , %d", e, N));
     System.out.println(String.format("Private: %d", d));
   }
 
-  private static int gcd(int e, int phi) {
-    int temp;
-    while (true) {
-      temp = (e % phi);
-      if (temp == 0) {
-        return phi;
-      }
-      e = phi;
-      phi = temp;
-    }
+  public byte[] encrypt(byte[] message) {
+    // x = (message ^ e) % n
+    return (new BigInteger(message)).modPow(e, N).toByteArray();
   }
 
-  public double encrypt(double msg, int e, int n) {
-    // Encryption c = (msg ^ e) % n
-    double c = Math.pow(msg, e);
-    c = (c % n);
-    return c;
-  }
-
-  public double decrypt(double msg, int d, int n) {
-    // Decryption c = (msg ^ d) % n
-    double c = Math.pow(msg, d);
-    c = (c % n);
-    return c;
+  public byte[] decrypt(byte[] message) {
+    // x = (message ^ d) % n
+    return (new BigInteger(message)).modPow(d, N).toByteArray();
   }
 }
